982961ed898c2f80f0ed26510e53a6ece37d3f98
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2019-11-15 15:06:26 +0100

    yada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+import { HelloComponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'v9ViewChildren';
+export class AppComponent implements AfterViewInit {
+  name = 'v9ViewChildren';
+
+  @ViewChildren(HelloComponent) hellos: QueryList<any>;
+
+  ngAfterViewInit() {
+    this.hellos.forEach(hello => console.log(hello));
+  }
 }
