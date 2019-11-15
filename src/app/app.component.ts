c8b43bdbf6638a6632d69bd2a812a2be3578e002
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2019-11-15 15:10:42 +0100

    queries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,20 @@ import { HelloComponent } from './hello/hello.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  queries: {
+    hellosQuery: new ViewChildren(HelloComponent)
+  }
 })
 export class AppComponent implements AfterViewInit {
-  name = 'v9ViewChildren';
+  public name = 'v9ViewChildren';
+  public hellosQuery: QueryList<HelloComponent>;
 
-  @ViewChildren(HelloComponent) hellos: QueryList<any>;
+  @ViewChildren(HelloComponent) hellos: QueryList<any>; // Works
 
   ngAfterViewInit() {
-    this.hellos.forEach(hello => console.log(hello));
+    this.hellos.forEach(hello => console.log('PROPS', hello));
+
+    this.hellosQuery.forEach(hello => console.log('QUERIES', hello));
   }
 }
